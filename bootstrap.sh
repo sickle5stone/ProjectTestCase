b32d635c8887495a3e32b4b2de14f74da53155b0
sickle5stone <aisp.aloysius@gmail.com>
2016-07-12 19:41:31 +0800

    VagrantFile & Init Script
    Upload of vagrant and init script

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+#config.vm.provision :shell, path: "./bootstrap.sh"
+
+#installing services
+#PASSWORD='mysqlP4ss'
+
+#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+#sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+#sudo apt-get update
+#sudo apt-get -y install mysql-server
+#sudo mysql_secure_installation
+#sudo mysql_install_db
